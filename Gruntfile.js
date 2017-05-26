2a58794d760060e5472d1e25a9048803106c348b
Ralf S. Engelschall <rse@engelschall.com>
2017-05-26 13:31:45 +0200

    initially place under version control

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+/*
+**  graphql-query-compress -- Compress a GraphQL Query String
+**  Copyright (c) 2017 Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/* global module: true */
+module.exports = function (grunt) {
+    grunt.loadNpmTasks("grunt-contrib-clean")
+    grunt.loadNpmTasks("grunt-browserify")
+    grunt.loadNpmTasks("grunt-eslint")
+    grunt.initConfig({
+        eslint: {
+            options: {
+                configFile: "eslint.yaml"
+            },
+            "gruntfile": [ "Gruntfile.js" ],
+            "graphql-query-compress": [ "src/**/*.js" ]
+        },
+        browserify: {
+            "graphql-query-compress-browser": {
+                files: {
+                    "lib/graphql-query-compress.browser.js": [ "src/**/*.js" ]
+                },
+                options: {
+                    transform: [
+                        [ "babelify", {
+                            presets: [ "es2015", "es2016", "es2017" ],
+                            plugins: [ [ "transform-runtime", {
+                                "polyfill":    true,
+                                "regenerator": true
+                            } ] ]
+                        } ]
+                    ],
+                    plugin: [
+                        [ "minifyify", { map: "graphql-query-compress.browser.map", output: "lib/graphql-query-compress.browser.map" } ],
+                        [ "browserify-derequire" ],
+                        [ "browserify-header" ]
+                    ],
+                    browserifyOptions: {
+                        standalone: "GraphQLFrontend",
+                        debug: true
+                    }
+                }
+            },
+            "graphql-query-compress-node": {
+                files: {
+                    "lib/graphql-query-compress.node.js": [ "src/**/*.js" ]
+                },
+                options: {
+                    transform: [
+                        [ "babelify", { presets: [ "es2015", "es2016", "es2017" ] } ]
+                    ],
+                    plugin: [
+                        [ "browserify-derequire" ],
+                        [ "browserify-header" ]
+                    ],
+                    external: [
+                        "tokenizr"
+                    ],
+                    browserifyOptions: {
+                        standalone: "GraphQLFrontend",
+                        debug: false
+                    }
+                }
+            }
+        },
+        clean: {
+            clean: [],
+            distclean: [ "node_modules" ]
+        }
+    })
+    grunt.registerTask("default", [ "eslint", "browserify" ])
+}
+
---
+++ src/graphql-query-compress.js
@@ -0,0 +1,77 @@
+/*
+**  graphql-query-compress -- Compress a GraphQL Query String
+**  Copyright (c) 2017 Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*  external dependency  */
+import Tokenizr from "tokenizr"
+
+/*  the APU function: compress a GraphQL query string  */
+function compactGraphQLQuery (query) {
+    let lexer = new Tokenizr()
+
+    /*  configure lexical analysis  */
+    lexer.rule(/#[^\r\n]*(?=\r?\n)/,                       (ctx, match) => { ctx.accept("comment") })
+    lexer.rule(/"(?:\\"|[^\r\n]+)*"/,                      (ctx, match) => { ctx.accept("string") })
+    lexer.rule(/$[a-zA-Z_][a-zA-Z0-9_]*/,                  (ctx, match) => { ctx.accept("var") })
+    lexer.rule(/[a-zA-Z_][a-zA-Z0-9_]*/,                   (ctx, match) => { ctx.accept("id") })
+    lexer.rule(/[+-]?[0-9]*\.?[0-9]+(?:[eE][+-]?[0-9]+)?/, (ctx, match) => { ctx.accept("number") })
+    lexer.rule(/[ \t\r\n]+/,                               (ctx, match) => { ctx.accept("ws", " ") })
+    lexer.rule(/[{}]/,                                     (ctx, match) => { ctx.accept("brace") })
+    lexer.rule(/[[\]]/,                                    (ctx, match) => { ctx.accept("bracket") })
+    lexer.rule(/[()]/,                                     (ctx, match) => { ctx.accept("parenthesis") })
+    lexer.rule(/,/,                                        (ctx, match) => { ctx.accept("comma") })
+    lexer.rule(/!/,                                        (ctx, match) => { ctx.accept("not") })
+    lexer.rule(/\.\.\./,                                   (ctx, match) => { ctx.accept("ellipsis") })
+    lexer.rule(/@/,                                        (ctx, match) => { ctx.accept("at") })
+    lexer.rule(/:/,                                        (ctx, match) => { ctx.accept("colon") })
+    lexer.rule(/./,                                        (ctx, match) => { ctx.accept("any") })
+    lexer.input(query)
+
+    /*  fetch all parsed tokens  */
+    let tokens = lexer.tokens()
+
+    /*  remove whitespace tokens at harmless positions  */
+    let output = ""
+    let re = /^(?:brace|bracket|parenthesis|comma|colon)$/
+    for (let i = 0; i < tokens.length; i++) {
+        if (   tokens[i].type === "comment"
+            || (   tokens[i].type === "ws"
+                && (   (   i < tokens.length - 1
+                        && tokens[i + 1].type.match(re))
+                    || (    i > 0
+                        && tokens[i - 1].type.match(re))))) {
+            tokens.splice(i, 1)
+            i--
+        }
+    }
+
+    /*  assembly and return new query string  */
+    tokens.forEach((token) => {
+        output += token.value
+    })
+    return output
+}
+
+/*  export the API function  */
+module.exports = compactGraphQLQuery
+
